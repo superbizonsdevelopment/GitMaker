16f44b68c3d783bd8d555d11ab22d07c0224a442
Anthony <antoni.p.bednarski@gmail.com>
2018-09-08 10:47:58 +0200

    Add cooperation with githubapi for organizations

--- server/handler/mainHandler.go
+++ server/handler/mainHandler.go
@@ -1,16 +1,39 @@
 package handler
 
 import (
-  "net/http"
-  "fmt"
+	"context"
+	"fmt"
+	"github.com/google/go-github/github"
+	"net/http"
 )
 
 func HandleMainFunction(w http.ResponseWriter, r *http.Request) {
-  if r.Method == http.MethodGet {
-    return
-  }
+	if r.Method == http.MethodGet {
+		return
+	}
 
-  if r.Method == http.MethodPost {
-    fmt.Fprintf(w,"Name: %v, SecondName: %v\n",r.PostFormValue("name"), r.PostFormValue("sname"))
-  }
+	if r.Method == http.MethodPost {
+		repoName := r.PostFormValue("name")
+		repoAuthor := r.PostFormValue("author")
+		repoIsAuthorIsAnOrganization := r.PostFormValue("isOrganization")
+
+		fmt.Fprintf(w, "Name: %v, Author: %v, isOrganization: %v\n", repoName, repoAuthor, repoIsAuthorIsAnOrganization)
+
+		if repoIsAuthorIsAnOrganization == "yes" {
+			//If author is an organization
+
+			client := github.NewClient(nil)
+
+			opt := &github.RepositoryListByOrgOptions{Type: "public"}
+
+			repos, _, _ := client.Repositories.ListByOrg(context.Background(), repoAuthor, opt)
+
+			for _, repo := range repos {
+				fmt.Printf("[DEBUG] Repo %s: %s\n", *repo.Owner.Login, *repo.ReleasesURL)
+			}
+
+		} else if repoIsAuthorIsAnOrganization == "no" {
+
+		}
+	}
 }
