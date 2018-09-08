0c1111373454f56c772460101881efee26b6e5c0
Anthony <antoni.p.bednarski@gmail.com>
2018-09-08 14:18:01 +0200

    Add isRepositoryExists safety to handler.

--- server/handler/mainHandler.go
+++ server/handler/mainHandler.go
@@ -7,6 +7,10 @@ import (
 	"net/http"
 )
 
+var (
+  repoLink = ""
+)
+
 func HandleMainFunction(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		return
@@ -20,20 +24,34 @@ func HandleMainFunction(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Name: %v, Author: %v, isOrganization: %v\n", repoName, repoAuthor, repoIsAuthorIsAnOrganization)
 
 		if repoIsAuthorIsAnOrganization == "yes" {
-			//If author is an organization
+			//If author is an organnization
 
-			client := github.NewClient(nil)
+	     isRXOOA, repoLink := isRepositoryExistOnOrganizationAccount(repoName)
 
-			opt := &github.RepositoryListByOrgOptions{Type: "public"}
+       if isRXOOA == false {
+         //send packet then repo isn't exist!
+         return
+       }
 
-			repos, _, _ := client.Repositories.ListByOrg(context.Background(), repoAuthor, opt)
-
-			for _, repo := range repos {
-				fmt.Printf("[DEBUG] Repo %s: %s\n", *repo.Owner.Login, *repo.ReleasesURL)
-			}
+       //switch on sh program with link to repo argument
 
 		} else if repoIsAuthorIsAnOrganization == "no" {
 
 		}
 	}
+}
+
+func isRepositoryExistOnOrganizationAccount(repoName string) (bool, string) {
+  client := github.NewClient(nil)
+
+  opt := &github.RepositoryListByOrgOptions{Type: "public"}
+
+  repos, _, _ := client.Repositories.ListByOrg(context.Background(), repoAuthor, opt)
+
+  for _, repo := range repos {
+    if repoName == *repo.Name {
+      return true, *repo.URL
+    }
+  }
+  return false, nil
 }
